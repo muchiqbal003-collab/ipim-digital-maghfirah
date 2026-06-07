67605d8fbe456bce50a8affb9c1379ef1fe22b82
muchiqbal003-collab <muchiqbal003@gmail.com>
2026-06-07 07:18:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-const CACHE_NAME = 'ipim-absensi-v1'
+const CACHE_NAME = 'ipim-absensi-v1';
+
 const urlsToCache = [
-  '/absensi-kampus/',
-  '/absensi-kampus/index.html',
-  '/absensi-kampus/absensi.html',
-  '/absensi-kampus/manifest.json',
-  '/absensi-kampus/images/icon-192.png',
-  '/absensi-kampus/images/icon-512.png'
-]
+  '/ipim-digital-maghfirah/',
+  '/ipim-digital-maghfirah/index.html',
+  '/ipim-digital-maghfirah/absensi.html',
+  '/ipim-digital-maghfirah/dashboard.html',
+  '/ipim-digital-maghfirah/manifest.json',
+  '/ipim-digital-maghfirah/style.css',
+  '/ipim-digital-maghfirah/images/icon-192.png',
+  '/ipim-digital-maghfirah/images/icon-512.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  )
-})
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  )
-})
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
